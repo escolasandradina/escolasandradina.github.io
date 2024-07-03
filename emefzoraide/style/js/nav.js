f1460377ac7a5c448b65d47459d6e1f4341a07ca
Alberto <albertoalfredo.profissional@gmail.com>
2024-07-03 11:44:53 -0300

    Criado navbar com efeito, criado banner e inicio do primeiro bloco

--- emefzoraide/style/js/nav.js
+++ emefzoraide/style/js/nav.js
@@ -3,6 +3,7 @@ window.onscroll = function() {myFunction()};
 
 // Get the navbar
 var navbar = document.getElementById("navbar");
+var navbarLogo = document.getElementById("nav-logo")
 
 // Get the offset position of the navbar
 var sticky = navbar.offsetTop;
@@ -10,8 +11,10 @@ var sticky = navbar.offsetTop;
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.scrollY >= sticky) {
-    navbar.classList.add("sticky")
+    navbar.classList.add("sticky");
+    navbarLogo.classList.add("stycky-navlogo");
   } else {
     navbar.classList.remove("sticky");
+    navbarLogo.classList.remove("stycky-navlogo")
   }
 }
